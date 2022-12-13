e3590d8778eacf330adfd63d89654f8a83dc1278
BuildTools <github@maxmailbox.de>
2022-12-13 14:58:13 +0100

    initial version of fib and fac

---
+++ 03.23/fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int n, fib;
+    int fib1 = -1;
+    int fib2 = 1;
+
+    printf("Bitte die Anzahl von Fibonnaci Zahlen/Folgen eingeben: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        fib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib;
+        printf(" %d", fib);
+        n --;
+    }
+
+    return 0;
+}
