0072fc74fd23b20dc550b1812dd96e4465986375
BuildTools <github@maxmailbox.de>
2022-12-13 15:16:04 +0100

    added 0

--- 03.23/fibonacci.c
+++ 03.23/fibonacci.c
@@ -3,12 +3,14 @@
 
 int main(){
 
-    int n, fib;
+    int n = 0, fib = 0;
     int fib1 = -1;
     int fib2 = 1;
 
     printf("Bitte die Anzahl von Fibonnaci Zahlen/Folgen eingeben: ");
     scanf("%d", &n);
+
+    (n == 0 ) && (printf("0\n"));
 
     while (n > 0)
     {
