9e0921df641485815e859f8a3cf909331f9f97eb
BuildTools <github@maxmailbox.de>
2023-01-03 13:11:32 +0100

    added improved version

---
+++ 03.26/primes_msander.c
@@ -0,0 +1,35 @@
+/*
+ *
+ * Calculate prime numbers (Maximilian Sander)
+ * respectfully ripped from
+ * Simon Grischek FOM Cologne KPP DTAG WiSe 2022
+ * cfranks ../tmp/03-26/prime.c
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool prime_check(int x){
+    //randbedingung
+    if(x<=1) return false;
+
+    for(int i=2; i < x; i++){
+        //teilbarkeit von x durch i
+        //i alles kleiner x von 2, def prime
+        if(x%i == 0) return false;
+    }
+    //nicht teilbar und nicht 1
+    return true;
+}
+
+void main(){
+    //Start
+    int n = 300;
+    //print start
+    printf("Startwert: %d\n", n);
+    //vom Start rückwärts, sonst von 2 bis <=n i++
+    for(int i=300; i >= 2; i--){
+        if(prime_check(i)) printf("%d ", i);
+    }
+    printf("\n");
+}
