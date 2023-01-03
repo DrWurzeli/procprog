1c45f3855e3031b02f9a22e720043f56e5f9a32b
BuildTools <github@maxmailbox.de>
2023-01-03 18:33:16 +0100

    added comment

--- 04.03/main.c
+++ 04.03/main.c
@@ -1,3 +1,9 @@
+/*
+Lies den Radius eines Kreises ein. Es soll der Umfang
+und der Flächeninhalt berechnet werden.
+Lagere die Funktionen und ihre Deklarationen in ein gesondertes .h/.cDateienpaar aus
+und erstelle ein entsprechendes Makefile.*/
+
 #include "circle.h"
 
 void main(){
