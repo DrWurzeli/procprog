b5b92db89bdfd36952738e3d953b764f2f939049
BuildTools <github@maxmailbox.de>
2023-01-03 14:27:38 +0100

    final

--- 04.02/smaller_bigger_equal.c
+++ 04.02/smaller_bigger_equal.c
@@ -26,6 +26,6 @@ int eingabeZahl() {
 
 void vergleich(int a, int b){
     if (a > b) printf("Erste Zahl ist groesser.\n");
-    else if (a == b) printf("Erste Zahl ist gleich.\n");
+    else if (a == b) printf("Zahlen sind gleich.\n");
     else printf("Erste Zahl ist kleiner.");
 }
