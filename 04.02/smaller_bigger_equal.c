38c1a035632f65a767efd8c65fc917cbff8437e9
BuildTools <github@maxmailbox.de>
2023-01-03 14:25:15 +0100

    inital version

---
+++ 04.02/smaller_bigger_equal.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/*
+Zwei Zahlen einlesen a b, ist a gleich größer oder kleiner als b?
+*/
+
+// Funktions-Prototypen
+int eingabeZahl();
+void vergleich(int a, int b);
+
+// Hauptprogramm
+void main() {
+    int a = eingabeZahl();
+    int b = eingabeZahl();
+    // Rechenvorgang
+    vergleich(a, b);
+}
+
+// Funktionen
+int eingabeZahl() {
+    int eingabe;
+    printf("Eingabe Zahl: ");
+    scanf("%d", &eingabe);
+    return eingabe;
+}
+
+void vergleich(int a, int b){
+    if (a > b) printf("Erste Zahl ist groesser.\n");
+    else if (a == b) printf("Erste Zahl ist gleich.\n");
+    else printf("Erste Zahl ist kleiner.");
+}
