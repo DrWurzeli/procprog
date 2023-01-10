584303791e0887e9abf7a6e2532dfb61455abcd4
Maximilian Sander <github@maxmailbox.de>
2023-01-10 14:26:07 +0100

    inital version

---
+++ 06.04/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(){
+    int const start = 298;
+    int sieb[start];
+    //printf("Startwert: %d ", start);
+
+    for(int i=0; i < start; i++){
+        sieb[i] = i+2;
+        //printf("%d", sieb[i]);
+    }
+
+    for(int j=0; j<start; j++){
+        if(sieb[j] != 0){
+            printf("%d ", sieb[j]);
+            for(int k=2; k*sieb[j]<start+2;k++){
+                    sieb[((k*sieb[j])-2)] = 0;
+                }
+        }
+        else{
+            continue;
+        }
+    }
+
+    printf("\n");
+}
