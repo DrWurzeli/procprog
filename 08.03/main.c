41e21587bc310e65aec6295789e45fd00cfea07a
BuildTools <github@maxmailbox.de>
2023-01-16 18:06:23 +0100

    fixed alignment and cleaned code

--- 08.03/main.c
+++ 08.03/main.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
+/*
+Das Programm erstellt zwei Arrays von Strings, und formatiert eine Ausgabe
+von konstant 80 Zeichen sodass alles schön gleichmäßig ist.
+Basically ein Test der String Funktionen oder so, idk.
+*/
+
 int main(){
 
     char* vorname[5] = {"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"};
     char* nachname[5] = {"Eins", "Zwei", "Drei", "Vier", "Fuenf"};
 
     for (int i=0; i<5; i++){
-        int laenge1, laenge2, space;
-
-        laenge1 = strlen(vorname[i]);
-        laenge2 = strlen(nachname[i]);
-        space = 79-laenge1-laenge2;
-
-        char fill[space];
-        for(int j=0; j<=space; j++){
-            fill[j] = '_';
-        }
+        int len1 = strlen(vorname[i]);
+        int len2 = strlen(nachname[i]);
+        int space = 80 - len1 - len2;
 
         char ausgabe[81];
+
         strcpy(ausgabe, vorname[i]);
-        strcat(ausgabe, fill);
+
+        for(int j=0; j<space; j++){
+            strcat(ausgabe, "_");
+        }
         strcat(ausgabe, nachname[i]);
         printf("%s\n", ausgabe);
 
