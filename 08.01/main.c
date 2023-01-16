68586e9650df9367b37c2508c712b8bb3cef65ca
Maximilian Sander <github@maxmailbox.de>
2023-01-16 15:02:07 +0100

    initial push

---
+++ 08.01/main.c
@@ -0,0 +1,11 @@
+/*
+Initialisiere ein paar Arrays mit Produktname, Packungsgröße in Gramm und Preis in Euro.
+Gib eine Liste mit diesen Werten aus:
+Produktname, Packungsgröße, Kilopreis netto, Kilopreis brutto, in Euro. 7% USt
+*/
+#include "main.h"
+
+int main(){
+
+    return EXIT_SUCCESS;
+}
