7fea75f18166290aab9dfb12b16477f17c89d9ec
Maximilian Sander <github@maxmailbox.de>
2023-01-17 14:25:53 +0100

    v3.1

--- 08.04/main.c
+++ 08.04/main.c
@@ -4,18 +4,15 @@ int main(){
 
     char string[] = "C:\\Eigene Dateien\\FOM\\C-Code\\main.c";
 
-    if(strrchr(string, '.') != NULL){
-        char *p = strrchr(string, '.')+1;
-        printf("Dateiendung: %s\n", strstr(string, p));
+    char *p = strrchr(string, '.')+1;
+    printf("Dateiendung: %s\n", strstr(string, p));
 
-        p = strrchr(string, '\\')+1;
-        printf("Dateiname: %s\n", strstr(string, p));
+    p = strrchr(string, '\\')+1;
+    printf("Dateiname: %s\n", strstr(string, p));
 
-        p -= 1;
-        *p = '\0';
-        printf("Verzeichnis: %s", string);
-    }
-
+    p -= 1;
+    *p = '\0';
+    printf("Verzeichnis: %s", string);
 
     printf("\n");
     return EXIT_SUCCESS;
